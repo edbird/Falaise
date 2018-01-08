5b9abe18deefd50cb392afda9e6c5fa8b9fd8dd2
ecb <ecb@hostname.hep.manchester.ac.uk>
2018-01-08 16:08:32 +0000

    slightly improved formatting using online tool

--- source/falaise/snemo/datamodels/calibrated_tracker_hit.ipp
+++ source/falaise/snemo/datamodels/calibrated_tracker_hit.ipp
@@ -23,40 +23,40 @@ namespace datamodel {
 
 template <class Archive>
 void calibrated_tracker_hit::serialize(Archive& ar_, const unsigned int version_) {
-  ar_& BOOST_SERIALIZATION_BASE_OBJECT_NVP(base_hit);
-  if (version_ == 0 && Archive::is_loading::value) {
-    _traits_ = 0x0;
+    ar_& BOOST_SERIALIZATION_BASE_OBJECT_NVP(base_hit);
+    if (version_ == 0 && Archive::is_loading::value) {
+        _traits_ = 0x0;
+        ar_& boost::serialization::make_nvp("r", _r_);
+        ar_& boost::serialization::make_nvp("sigma_r", _sigma_r_);
+        ar_& boost::serialization::make_nvp("z", _z_);
+        ar_& boost::serialization::make_nvp("sigma_z", _sigma_z_);
+        datatools::invalidate(_x_);
+        datatools::invalidate(_y_);
+        ar_& boost::serialization::make_nvp("x", _x_);
+        ar_& boost::serialization::make_nvp("y", _y_);
+        if (datatools::is_valid(_x_) && datatools::is_valid(_y_)) {
+            _set_trait_bit(true, xy);
+        }
+        datatools::invalidate(_delayed_time_);
+        datatools::invalidate(_delayed_time_error_);
+        return;
+    }
+
+    // From version 1 :
+    ar_& boost::serialization::make_nvp("traits", _traits_);
     ar_& boost::serialization::make_nvp("r", _r_);
     ar_& boost::serialization::make_nvp("sigma_r", _sigma_r_);
     ar_& boost::serialization::make_nvp("z", _z_);
     ar_& boost::serialization::make_nvp("sigma_z", _sigma_z_);
-    datatools::invalidate(_x_);
-    datatools::invalidate(_y_);
-    ar_& boost::serialization::make_nvp("x", _x_);
-    ar_& boost::serialization::make_nvp("y", _y_);
-    if (datatools::is_valid(_x_) && datatools::is_valid(_y_)) {
-      _set_trait_bit(true, xy);
+    if (has_xy()) {
+        ar_& boost::serialization::make_nvp("x", _x_);
+        ar_& boost::serialization::make_nvp("y", _y_);
     }
-    datatools::invalidate(_delayed_time_);
-    datatools::invalidate(_delayed_time_error_);
+    if (is_delayed()) {
+        ar_& boost::serialization::make_nvp("delayed_time", _delayed_time_);
+        ar_& boost::serialization::make_nvp("delayed_time_error", _delayed_time_error_);
+    }
     return;
-  }
-
-  // From version 1 :
-  ar_& boost::serialization::make_nvp("traits", _traits_);
-  ar_& boost::serialization::make_nvp("r", _r_);
-  ar_& boost::serialization::make_nvp("sigma_r", _sigma_r_);
-  ar_& boost::serialization::make_nvp("z", _z_);
-  ar_& boost::serialization::make_nvp("sigma_z", _sigma_z_);
-  if (has_xy()) {
-    ar_& boost::serialization::make_nvp("x", _x_);
-    ar_& boost::serialization::make_nvp("y", _y_);
-  }
-  if (is_delayed()) {
-    ar_& boost::serialization::make_nvp("delayed_time", _delayed_time_);
-    ar_& boost::serialization::make_nvp("delayed_time_error", _delayed_time_error_);
-  }
-  return;
 }
 
 }  // end of namespace datamodel
--- source/falaise/snemo/processing/mock_tracker_s2c_module.h
+++ source/falaise/snemo/processing/mock_tracker_s2c_module.h
@@ -48,87 +48,87 @@ namespace processing {
 /// \brief Simple modelling of the time and space measurement with the SuperNEMO drift cells in
 /// Geiger mode
 class mock_tracker_s2c_module : public dpp::base_module {
- public:
-  /// Collection of raw tracker hit Intermediate :
-  typedef std::list<snemo::datamodel::mock_raw_tracker_hit> raw_tracker_hit_col_type;
+public:
+    /// Collection of raw tracker hit Intermediate :
+    typedef std::list<snemo::datamodel::mock_raw_tracker_hit> raw_tracker_hit_col_type;
 
-  /// Set the geometry manager
-  void set_geom_manager(const geomtools::manager& gmgr_);
+    /// Set the geometry manager
+    void set_geom_manager(const geomtools::manager& gmgr_);
 
-  /// Getting geometry manager
-  const geomtools::manager& get_geom_manager() const;
+    /// Getting geometry manager
+    const geomtools::manager& get_geom_manager() const;
 
-  /// Set the external PRNG
-  void set_external_random(mygsl::rng& rng_);
+    /// Set the external PRNG
+    void set_external_random(mygsl::rng& rng_);
 
-  /// Reset the external PRNG
-  void reset_external_random();
+    /// Reset the external PRNG
+    void reset_external_random();
 
-  /// Check if the module use an external PRNG
-  bool has_external_random() const;
+    /// Check if the module use an external PRNG
+    bool has_external_random() const;
 
-  /// Return the drift time threshold for peripheral Geiger hits (far from the anode wire)
-  double get_peripheral_drift_time_threshold() const;
+    /// Return the drift time threshold for peripheral Geiger hits (far from the anode wire)
+    double get_peripheral_drift_time_threshold() const;
 
-  /// Return the drift time threshold for delayed Geiger hits
-  double get_delayed_drift_time_threshold() const;
+    /// Return the drift time threshold for delayed Geiger hits
+    double get_delayed_drift_time_threshold() const;
 
-  /// Constructor
-  mock_tracker_s2c_module(datatools::logger::priority = datatools::logger::PRIO_FATAL);
+    /// Constructor
+    mock_tracker_s2c_module(datatools::logger::priority = datatools::logger::PRIO_FATAL);
 
-  /// Destructor
-  virtual ~mock_tracker_s2c_module();
+    /// Destructor
+    virtual ~mock_tracker_s2c_module();
 
-  /// Initialization
-  virtual void initialize(const datatools::properties& setup_,
-                          datatools::service_manager& service_manager_,
-                          dpp::module_handle_dict_type& module_dict_);
+    /// Initialization
+    virtual void initialize(const datatools::properties& setup_,
+                            datatools::service_manager& service_manager_,
+                            dpp::module_handle_dict_type& module_dict_);
 
-  /// Reset
-  virtual void reset();
+    /// Reset
+    virtual void reset();
 
-  /// Data record processing
-  virtual process_status process(datatools::things& data_);
+    /// Data record processing
+    virtual process_status process(datatools::things& data_);
 
- protected:
-  /// Set default attributes values
-  void _set_defaults();
+protected:
+    /// Set default attributes values
+    void _set_defaults();
 
-  /// Getting random number generator
-  mygsl::rng& _get_random();
+    /// Getting random number generator
+    mygsl::rng& _get_random();
 
-  /// Digitize tracker hits
-  void _process_tracker_digitization(const mctools::simulated_data& simulated_data_,
-                                     raw_tracker_hit_col_type& raw_tracker_hits_);
+    /// Digitize tracker hits
+    void _process_tracker_digitization(const mctools::simulated_data& simulated_data_,
+                                       raw_tracker_hit_col_type& raw_tracker_hits_);
 
-  /// Calibrate tracker hits (longitudinal and transverse spread)
-  void _process_tracker_calibration(
-      const raw_tracker_hit_col_type& raw_tracker_hits_,
-      snemo::datamodel::calibrated_data::tracker_hit_collection_type& calibrated_tracker_hits_);
+    /// Calibrate tracker hits (longitudinal and transverse spread)
+    void _process_tracker_calibration(
+        const raw_tracker_hit_col_type& raw_tracker_hits_,
+        snemo::datamodel::calibrated_data::tracker_hit_collection_type& calibrated_tracker_hits_);
 
-  /// Main process function
-  void _process(
-      const mctools::simulated_data& simulated_data_,
-      snemo::datamodel::calibrated_data::tracker_hit_collection_type& calibrated_tracker_hits_);
+    /// Main process function
+    void _process(
+        const mctools::simulated_data& simulated_data_,
+        snemo::datamodel::calibrated_data::tracker_hit_collection_type& calibrated_tracker_hits_);
 
- private:
-  const geomtools::manager* _geom_manager_;  //!< The geometry manager
-  std::string _module_category_;             //!< The geometry category of the SuperNEMO module
-  std::string _hit_category_;                //!< The category of the input Geiger hits
-  geiger_regime _geiger_;                    //!< Geiger regime tools
-  mygsl::rng _random_;                       //!< internal PRN generator
-  mygsl::rng* _external_random_;             //!< external PRN generator
-  double _peripheral_drift_time_threshold_;  //!< Peripheral drift time threshold
-  double _delayed_drift_time_threshold_;     //!< Delayed drift time threshold
-  std::string _SD_label_;                    //!< The label of the simulated data bank
-  std::string _CD_label_;                    //!< The label of the calibrated data bank
-  std::string _Geo_label_;                   //!< The label of the geometry service
-  bool _store_mc_hit_id_;                    //!< Flag to store the MC true hit ID
-  bool _store_mc_truth_track_ids_;  //!< The flag to reference the MC engine track and parent track
-                                    //!< IDs associated to this calibrated Geiger hit
+private:
+    const geomtools::manager* _geom_manager_;  //!< The geometry manager
+    std::string _module_category_;             //!< The geometry category of the SuperNEMO module
+    std::string _hit_category_;                //!< The category of the input Geiger hits
+    geiger_regime _geiger_;                    //!< Geiger regime tools
+    mygsl::rng _random_;                       //!< internal PRN generator
+    mygsl::rng* _external_random_;             //!< external PRN generator
+    double _peripheral_drift_time_threshold_;  //!< Peripheral drift time threshold
+    double _delayed_drift_time_threshold_;     //!< Delayed drift time threshold
+    std::string _SD_label_;                    //!< The label of the simulated data bank
+    std::string _CD_label_;                    //!< The label of the calibrated data bank
+    std::string _Geo_label_;                   //!< The label of the geometry service
+    bool _store_mc_hit_id_;                    //!< Flag to store the MC true hit ID
+    bool _store_mc_truth_track_ids_;  //!< The flag to reference the MC engine track and parent track
+    //!< IDs associated to this calibrated Geiger hit
 
-  // Macro to automate the registration of the module :
-  DPP_MODULE_REGISTRATION_INTERFACE(mock_tracker_s2c_module)
+    // Macro to automate the registration of the module :
+    DPP_MODULE_REGISTRATION_INTERFACE(mock_tracker_s2c_module)
 };
 
 }  // end of namespace processing
